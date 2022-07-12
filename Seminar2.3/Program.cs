5f98c95b920cd881ec1987d6ca0bca201084e9c5
Alexander <masalov.alex1996@gmail.com>
2022-07-13 02:54:49 +0500

    Написал последнюю программу с семинара самостоятельно

---
+++ Seminar2.3/Program.cs
@@ -0,0 +1,19 @@
+﻿//Напишите программу, которая принимает
+//на вход два числа и проверяет, является ли одно число квадратом другого.
+Console.WriteLine("Введите первое число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int num2 = Convert.ToInt32(Console.ReadLine());
+string Cheaker(int num1, int num2)
+{
+    if( num2%num1 == 0 && num1%num2 != 0 )
+    {
+        return "Второе число является квадратом первого числа";
+    }
+    else
+    {
+        return "Первое число является квадратом второго числа";
+    }
+
+}
+Console.WriteLine(Cheaker(num1,num2));
