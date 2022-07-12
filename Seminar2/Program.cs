001592259ff84ca169b8f1509d4adf1815b1f430
Alexander <masalov.alex1996@gmail.com>
2022-07-12 23:19:53 +0500

    Сделал самостоятельно вторую задачу на семинаре

---
+++ Seminar2/Program.cs
@@ -0,0 +1,12 @@
+﻿//Напишите программу, 
+//которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+int ShowNumber(){
+    int num = new Random().Next(100,999);
+    Console.Write(num+"->");
+    int first = num/100;
+    Console.Write(first);
+    int deleteSecond = num%100;
+    int third = deleteSecond%10;
+    return third;
+}
+Console.WriteLine(ShowNumber());
