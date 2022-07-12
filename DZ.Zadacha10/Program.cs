e24ed8ffca61ac41325731c73d882c4fb98381a5
Alexander <masalov.alex1996@gmail.com>
2022-07-13 03:57:23 +0500

    Написал программу согласно 10 задаче из ДЗ

---
+++ DZ.Zadacha10/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая принимает 
+//на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.WriteLine("Введите трёхзначное число");
+int num = Convert.ToInt32(Console.ReadLine());
+int ShowSecNum(){
+
+    int first2 = num%100;
+    int Sec = first2/10;
+    Console.Write(num+"->");
+    return Sec;
+
+}
+Console.WriteLine(ShowSecNum());
