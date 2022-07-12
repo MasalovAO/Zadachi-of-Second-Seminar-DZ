4c0b5780242065c737cb01660274874c40ebbbf6
Alexander <masalov.alex1996@gmail.com>
2022-07-13 01:12:27 +0500

    Сделал вторую задачу самостоятельно, но частично, т.к. были ошибки

---
+++ Seminar/Program.cs
@@ -0,0 +1,20 @@
+﻿int ShowNumber(){
+    int num = new Random().Next(10,99);
+    int first = num/10;
+    int second = num%10;
+
+    Console.Write(num+"->");
+
+    if(first>second)
+    {
+        return first;
+    }    
+
+    else
+    {
+        return second;
+    }    
+    
+    
+}
+Console.WriteLine(ShowNumber());
---
+++ Seminar2.1/Program.cs
@@ -0,0 +1,23 @@
+﻿//Напишите программу, которая будет принимать на вход два числа и выводить, 
+//является ли второе число кратным первому. 
+//Если второе число не кратно числу первому, 
+//то программа выводит остаток от деления.
+Console.WriteLine("Введите первое число");
+double number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+double number2 = Convert.ToInt32(Console.ReadLine());
+string Search(double number1, double number2){
+    if(number2%number1 == 0){
+        
+        return "Второе число кратное первому";
+        
+        //Console.WriteLine(chastnoe-"Первое число кратно второму числу");
+        
+    }
+    else{
+        
+        return "Остаток " + number2%number1;
+
+    }
+}
+Console.WriteLine(Search(number1,number2));
