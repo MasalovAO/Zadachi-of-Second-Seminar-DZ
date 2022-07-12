5273b2bfb72a595c59eb3acd2db45308d555a11f
Alexander <masalov.alex1996@gmail.com>
2022-07-13 02:21:05 +0500

    Сделал вторую задачу, но немного по другому

---
+++ Seminar2.2/Program.cs
@@ -0,0 +1,23 @@
+﻿//Напишите программу, 
+//которая принимает на вход число и проверяет, 
+//кратно ли оно одновременно 7 и 23.
+
+
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+string Cheaker(int num)
+{
+    if(num%7 + num%23==0)
+    {
+        
+        return"Ваше число кратно 7 и 23";
+
+    }
+    else
+    {
+        return"Ваше число не кратно";
+    }
+
+}
+
+Console.WriteLine(Cheaker(num));
